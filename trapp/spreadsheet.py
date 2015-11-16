f3f6d9841a6b454eb18ba86482f55428bd9ad240
Matt Bernhardt <mjbernha@mit.edu>
2015-11-16 09:10:06 -0500

    Adds spreadsheet shim

---
+++ trapp/spreadsheet.py
@@ -0,0 +1,16 @@
+from xlrd import open_workbook
+
+
+class Spreadsheet():
+
+    def __init__(self, file):
+        self.data = open_workbook(file)
+
+    def fields(self):
+        # This assumes only one worksheet.
+        # It returns a list of the values across the top of the first worksheet.
+        self.fields = []
+        sheet = self.data.sheets()[0]
+        for col in range(sheet.ncols):
+            self.fields.append(sheet.cell(0, col).value)
+        return self.fields
