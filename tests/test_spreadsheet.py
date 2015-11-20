d1c76753638a3926d962679174e9ae9435db7a68
Matthew Bernhardt <mjbernha@mit.edu>
2015-11-19 21:28:17 -0500

    Adds excel fixture, configuration changes; this addresses #4

--- tests/test_spreadsheet.py
+++ tests/test_spreadsheet.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import
 from trapp.spreadsheet import Spreadsheet
 
 
-def test_spreadsheet_init():
-    s = Spreadsheet('test.xlsx')
+def test_spreadsheet_init(excel):
+    s = Spreadsheet(excel)
     assert isinstance(s, Spreadsheet)
-    assert isinstance(s.data, xlrd)
+    # assert isinstance(s.data, xlrd)
