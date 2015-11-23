1d7eae297dd7f49cc6ba8ac05cd50693446247d4
Matt Bernhardt <mjbernha@mit.edu>
2015-11-23 00:16:33 -0500

    Resets tabs and spaces, basic linting

--- trapp/command_line.py
+++ trapp/command_line.py
@@ -1,67 +1,66 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
-import sys
 import argparse
-import trapp
 from trapp.log import Log
 from trapp.importer import Importer
 
+
 def main():
-	# I'm imagining a few different verbs:
-	# import: harvest data out of supplied files
-	# - games
-	# - lineups
-	# - players
-	# - ...etc
-	#
-	# compile: internal database operations, building summary tables and other
-	#          calculated values
-	# - summary statistics (GP, GS, Min, G, A, etc)
-	# - plus/minus data (or other combinations)
-	# - ...etc
-	#
-	# render: generate csv files, excel spreadsheets, or other images based on
-	#         stored data
-	# - ...etc
-	#
-	# qa: consistency checks
-	#
+    # I'm imagining a few different verbs:
+    # import: harvest data out of supplied files
+    # - games
+    # - lineups
+    # - players
+    # - ...etc
+    #
+    # compile: internal database operations, building summary tables and other
+    #          calculated values
+    # - summary statistics (GP, GS, Min, G, A, etc)
+    # - plus/minus data (or other combinations)
+    # - ...etc
+    #
+    # render: generate csv files, excel spreadsheets, or other images based on
+    #         stored data
+    # - ...etc
+    #
+    # qa: consistency checks
+    #
 
-	# Initialize argument parser
-	parser = argparse.ArgumentParser(
-		description='Where does this appear?'
-	)
-	parser.add_argument(
-		'verb',
-		choices=['import','import-games','compile','render','qa'],
-	)
-	args = parser.parse_args()
+    # Initialize argument parser
+    parser = argparse.ArgumentParser(
+        description='Where does this appear?'
+    )
+    parser.add_argument(
+        'verb',
+        choices=['import', 'import-games', 'compile', 'render', 'qa'],
+    )
+    args = parser.parse_args()
 
-	if (args.verb == 'import'):
-		print('Importing...')
+    if (args.verb == 'import'):
+        print('Importing...')
 
-	elif (args.verb == 'import-games'):
-		print('Importing games...')
-		l = Log('trapp-import-games.log')
-		l.message('Beginning game import')
-		# Need to receive filename from command line
-		imp = Importer('c:/Users/mattj_000/Documents/GitHub/trapp/trapp/imports/games.xlsx')
-		# Check for required fields
-		requiredColumns = ([
-			'MatchTime',
-			'MatchTypeID',
-			'HTeamID',
-			'ATeamID'
-		])
-		l.end()
+    elif (args.verb == 'import-games'):
+        print('Importing games...')
+        l = Log('trapp-import-games.log')
+        l.message('Beginning game import')
+        # Need to receive filename from command line
+        imp = Importer('c:/Users/mattj_000/Documents/GitHub/trapp/trapp/imports/games.xlsx')
+        # Check for required fields
+        requiredColumns = ([
+            'MatchTime',
+            'MatchTypeID',
+            'HTeamID',
+            'ATeamID'
+        ])
+        l.end()
 
-	elif (args.verb == 'compile'):
-		print('Compiling...')
+    elif (args.verb == 'compile'):
+        print('Compiling...')
 
-	elif (args.verb == 'render'):
-		print('Rendering...')
+    elif (args.verb == 'render'):
+        print('Rendering...')
 
-	else:
-		# qa
-		print('Testing...')
+    else:
+        # qa
+        print('Testing...')
--- trapp/importer.py
+++ trapp/importer.py
@@ -5,12 +5,12 @@ from trapp.spreadsheet import Spreadsheet
 
 class Importer():
 
-	def __init__(self, importFile):
-		# This probably needs to check the submitted file type and read in the
-		# appropriate shim.
-		# For now, though, only Excel spreadsheets are supported.
-		self.source = Spreadsheet(importFile)
+    def __init__(self, importFile):
+        # This probably needs to check the submitted file type and read in the
+        # appropriate shim.
+        # For now, though, only Excel spreadsheets are supported.
+        self.source = Spreadsheet(importFile)
 
-	def checkFields(self, fields):
-		# This checks the imported spreadsheet for a dictionary of required fields
-		a = 1
+    def checkFields(self, fields):
+        # This checks the imported spreadsheet for a dictionary of required fields
+        return True
