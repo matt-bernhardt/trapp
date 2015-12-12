c5bfb7d1423aace04a3e3226b08e8f9a3318c6de
Matt Bernhardt <mjbernha@mit.edu>
2015-12-12 00:04:42 -0500

    Removes mock

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 from trapp.database import Database
-import mock
 
 
 def test_database_init():
@@ -14,44 +13,19 @@ def test_database_query_original():
     # This test works locally, but obviously can't on Travis without an
     # external database that has my actual content.
     d = Database()
-    d.connect() # This would need mocked
-    sql = ('SELECT ID '
-           'FROM tbl_players '
-           'WHERE FirstName = %s AND LastName = %s')
-    params = ('Brian', 'McBride', )
-    rs = d.query(sql, params) # This would need mocked, down to
-    if (rs.with_rows): #                                      |
-        records = rs.fetchall() #                             |
-    for term in records: #                                    |
-        needleID = records[0][0] #                       here ?
-    assert needleID == 1556
-    assert d.warnings() is None
-    d.disconnect() # This may need mocked?
-
-@mock.patch('trapp.database')
-def test_database_query(mock_database):
-    # Following http://www.toptal.com/python/an-introduction-to-mocking-in-python
-    #
-    # Questions:
-    #
-    # Q - Is that article even useful, since it builds on unittest not pytest? Same principle?
-    #
-    # Q - I'm not setting up a mock _test_, but a mock _method_ right?
-    #
-    # Q - Curious about how I'd mock a higher-level method (that ultimately depends on Database.query)
-    #     Would I mock 'query' differently in each case, standing in for what _would_ come back from the
-    #     database?
-    d = Database()
     d.connect()
     sql = ('SELECT ID '
            'FROM tbl_players '
            'WHERE FirstName = %s AND LastName = %s')
     params = ('Brian', 'McBride', )
-    # Q - the next line came fromm toptal - seems to checking how the mock method was called?
-    mock_database.query.assert_called_with(sql, params)
+    rs = d.query(sql, params)
+    if (rs.with_rows):
+        records = rs.fetchall()
+    for term in records:
+        needleID = records[0][0]
+    assert needleID == 1556
+    assert d.warnings() is None
     d.disconnect()
-    #
-    # Also reading https://pytest.org/latest/monkeypatch.html
 
 
 def test_database_convertDate():
