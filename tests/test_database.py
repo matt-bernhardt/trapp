92d2fa6c2f2b6c540156c0916bb1ca0626ea6ab4
Matt Bernhardt <mjbernha@mit.edu>
2015-12-12 09:29:57 -0500

    Expands test coverage of database object

--- tests/test_database.py
+++ tests/test_database.py
@@ -9,21 +9,33 @@ def test_database_init():
     assert d.cursor == ''
 
 
-def test_database_query_original():
-    # This test works locally, but obviously can't on Travis without an
-    # external database that has my actual content.
+def test_database_query():
     d = Database()
     d.connect()
+    # Inserts
+    sql = ('INSERT INTO tbl_players '
+           '(FirstName, LastName) '
+           'VALUES '
+           '(%s, %s)')
+    params = ('Brian', 'McBride', )
+    rs = d.query(sql, params)
+    assert d.warnings() is None
+    # Updates
+    sql = ('UPDATE tbl_players '
+           'SET Position = %s '
+           'WHERE FirstName = %s AND LastName = %s')
+    rs = d.query(sql, ('Forward', 'Brian', 'McBride', ))
+    assert d.warnings() is None
+    # Selects
     sql = ('SELECT ID '
            'FROM tbl_players '
            'WHERE FirstName = %s AND LastName = %s')
-    params = ('Brian', 'McBride', )
     rs = d.query(sql, params)
     if (rs.with_rows):
         records = rs.fetchall()
     for term in records:
         needleID = records[0][0]
-    assert needleID == 1556
+    assert needleID == 1
     assert d.warnings() is None
     d.disconnect()
 
