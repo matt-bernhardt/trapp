43097e905a797862e49fc360bffa418d6c926841
Matt Bernhardt <mjbernha@mit.edu>
2015-12-13 21:37:54 -0500

    Writes tests for ImporterPlayers and ImporterGames
    Signed-off-by: Matt Bernhardt <mjbernha@mit.edu>

--- tests/test_importer.py
+++ tests/test_importer.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import
 import pytest
 from trapp.log import Log
-from trapp.importer import Importer
+from trapp.importer import Importer, ImporterGames, ImporterPlayers
 
 
 def test_importer_init(excel):
@@ -10,6 +10,12 @@ def test_importer_init(excel):
     importer = Importer(excel, log)
     assert isinstance(importer, Importer)
     assert importer.log.name == 'test.log'
+
+
+def test_importer_correctValues(excel):
+    log = Log('test.log')
+    importer = Importer(excel, log)
+    assert importer.correctValues() is True
 
 
 def test_importer_checkData_sheets(excel_sheets):
@@ -54,3 +60,30 @@ def test_importer_setLog(excel):
     importer = Importer(excel, log)
     importer.setLog(log2)
     assert importer.log.name == 'test2.log'
+
+
+def test_importerGames(excel_games):
+    log = Log('test.log')
+    importer = ImporterGames(excel_games, log)
+    requiredColumns = ([
+        'MatchTime',
+        'MatchTypeID',
+        'HTeamID',
+        'ATeamID'
+    ])
+    assert importer.checkFields(requiredColumns) is True
+    assert importer.doImport() is True
+
+
+def test_importerPlayers(excel_players):
+    log = Log('test.log')
+    importer = ImporterPlayers(excel_players, log)
+    requiredColumns = ([
+        'FirstName',
+        'LastName',
+        'Position',
+        'DOB',
+        'Hometown'
+    ])
+    assert importer.checkFields(requiredColumns) is True
+    assert importer.doImport() is True
