ccbd87d0cc8ec1c6b4e833a7f22d9937f38ddcd9
Matt Bernhardt <mjbernha@mit.edu>
2016-01-05 00:16:53 -0500

    Adds saveDict method to GameMinute. Also adds tbl_gameminutes to testing framework

--- tests/test_gameminute.py
+++ tests/test_gameminute.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
+import pytest
 from trapp.gameminute import GameMinute
+from trapp.log import Log
 
 
 def test_gameminute_init():
@@ -24,3 +26,40 @@ def test_gameminute_disconnect():
     assert hasattr(gm, 'db')
     gm.disconnectDB()
     assert hasattr(gm, 'db') is False
+
+
+def test_gameminute_saveDict():
+    log = Log('test.log')
+    gm = GameMinute()
+    gm.connectDB()
+
+    # Formats
+    with pytest.raises(RuntimeError) as excinfo:
+        data = 'foo'
+        gm.saveDict(data, log)
+    assert 'saveDict requires a dictionary' in str(excinfo.value)
+
+    # Inserts
+    data = {
+        'MatchID': 1,
+        'TeamID': 1,
+        'PlayerID': 1,
+        'TimeOn': 0,
+        'TimeOff': 90,
+        'Ejected': 0
+    }
+    assert gm.saveDict(data, log) is True
+    assert gm.db.warnings() is None
+
+    # Updates
+    data = {
+        'ID': 1,
+        'MatchID': 2,
+        'TeamID': 2,
+        'PlayerID': 2,
+        'TimeOn': 0,
+        'TimeOff': 89,
+        'Ejected': 1
+    }
+    assert gm.saveDict(data, log) is True
+    assert gm.db.warnings() is None
--- trapp/gameminute.py
+++ trapp/gameminute.py
@@ -15,3 +15,46 @@ class GameMinute():
     def disconnectDB(self):
         self.db.disconnect()
         del self.db
+
+    def saveDict(self, data, log):
+        # Verify that data is a dictionary
+        if not (isinstance(data, dict)):
+            raise RuntimeError('saveDict requires a dictionary')
+
+        if ('ID' in data):
+            # Update
+            log.message('Record ID provided - we update')
+            sql = ('UPDATE tbl_gameminutes SET '
+                   'GameID = %s, '
+                   'TeamID = %s, '
+                   'PlayerID = %s, '
+                   'TimeOn = %s, '
+                   'TimeOff = %s, '
+                   'Ejected = %s '
+                   'WHERE ID = %s')
+            rs = self.db.query(sql, (
+                data['MatchID'],
+                data['TeamID'],
+                data['PlayerID'],
+                data['TimeOn'],
+                data['TimeOff'],
+                data['Ejected'],
+                data['ID']
+            ))
+        else:
+            log.message('No Record ID provided - we insert')
+            sql = ('INSERT INTO tbl_gameminutes '
+                   '(GameID, TeamID, PlayerID, TimeOn, TimeOff, Ejected)'
+                   'VALUES '
+                   '(%s, %s, %s, %s, %s, %s)')
+            rs = self.db.query(sql, (
+                data['MatchID'],
+                data['TeamID'],
+                data['PlayerID'],
+                data['TimeOn'],
+                data['TimeOff'],
+                data['Ejected']
+            ))
+            log.message(str(rs))
+
+        return True
