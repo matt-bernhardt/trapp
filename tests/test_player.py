14abb063a8e70cca61b6af78a83b5e392c3849b6
Matt Bernhardt <mjbernha@mit.edu>
2016-01-22 09:08:48 -0500

    Cleans up test coverage for Player object.
    Signed-off-by: Matt Bernhardt <mjbernha@mit.edu>

--- tests/test_player.py
+++ tests/test_player.py
@@ -53,6 +53,7 @@ def test_player_lookupID():
     # Setup
     log = Log('test.log')
     p = Player()
+    p.connectDB()
 
     # Format error
     with pytest.raises(RuntimeError) as excinfo:
@@ -70,9 +71,17 @@ def test_player_lookupID():
     assert 'Submitted data is missing the following fields' in str(excinfo.value)
 
     # Need a test of successful lookups
+    needle = {
+        'FirstName': 'Sample',
+        'LastName': 'Player',
+        'Position': 'Midfielder',
+        'DOB': (1980, 1, 1, 0, 0, 0, 0, 0, 0,),
+        'Hometown': 'Oneonta, NY'
+    }
+    assert p.lookupID(needle, log) == [15]
 
 
-def test_player_lookupIDbyGame():
+def test_player_lookupIDbyGoal():
     # Setup
     log = Log('test.log')
     p = Player()
@@ -92,6 +101,14 @@ def test_player_lookupIDbyGame():
         }
         p.lookupIDbyGoal(needle, log)
     assert 'Submitted data is missing the following fields' in str(excinfo.value)
+
+    # TODO: Need to actually look something up...
+    needle = {
+        'playername': 'Man',
+        'TeamID': 2,
+        'GameID': 1,
+    }
+    assert p.lookupIDbyGoal(needle, log) == [3]
 
 
 def test_player_lookupIDbyName():
@@ -115,15 +132,17 @@ def test_player_lookupIDbyName():
         p.lookupIDbyName(needle, log)
     assert 'Submitted data is missing the following fields' in str(excinfo.value)
 
+    # Look up a player we know doesn't exist.
     needle = {
         'PlayerName': 'asdf',
     }
     assert p.lookupIDbyName(needle, log) == []
 
+    # Look up a known player in the test dataset.
     needle = {
         'PlayerName': 'Sample Player',
     }
-    assert len(p.lookupIDbyName(needle, log)) == 1
+    assert p.lookupIDbyName(needle, log) == [15]
 
 
 def test_player_merge():
@@ -140,3 +159,5 @@ def test_player_saveDict():
         needle = 'Foo'
         p.saveDict(needle, log)
     assert 'saveDict requires a dictionary' in str(excinfo.value)
+
+    # TODO: Need to actually save something
