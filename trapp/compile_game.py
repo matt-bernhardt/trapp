5824df0759be8d2e0d72bb4e1249ea05d34f0712
Matt Bernhardt <mjbernha@mit.edu>
2016-02-06 23:39:58 -0500

    Adds method to look up all appearances, integrates with gamestats compiler

--- trapp/compile_game.py
+++ trapp/compile_game.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 from trapp.compiler import Compiler
+from trapp.gameminute import GameMinute
 
 
 class CompilerGames(Compiler):
@@ -9,6 +10,14 @@ class CompilerGames(Compiler):
         self.log.message('Running compile')
         # The game stats compiler does the following:
         # 1) Assemble list of player appearances
+        self.appearances = self.getAppearanceList()
+        for item in self.appearances:
+            self.log.message(str(item))
         # 2) For each appearance, calculate summary statistics
         # 3) Upsert those statistics as GameStat objects
         return True
+
+    def getAppearanceList(self):
+        gm = GameMinute()
+        gm.connectDB()
+        return gm.lookupIDlistByYear()
