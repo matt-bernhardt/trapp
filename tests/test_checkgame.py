c57db8897a35d17f55e1c9323e34402d46532141
Matt Bernhardt <mjbernha@mit.edu>
2016-04-10 21:37:21 -0400

    Adds test coverate to CheckerGames - should be 100%

--- tests/test_checkgame.py
+++ tests/test_checkgame.py
@@ -9,6 +9,25 @@ def test_checkerGames_init():
     log = Log('test.log')
     output = Log('test.csv')
     c = CheckerGames(log, output)
+    assert isinstance(c, CheckerGames)
+
+
+def test_checkerGames_checkGames():
+    log = Log('test.log')
+    output = Log('test.csv')
+    c = CheckerGames(log, output)
+    c.checkGames()
+    # Re-open output in read mode
+    output.file = open('test.csv', 'r')
+    # Default start year is 2012, but sample games are in 1980.
+    # This is by design, to give a negative test.
+    assert output.file.readline() == ''
+
+
+def test_checkerGames_reviewCompetition():
+    log = Log('test.log')
+    output = Log('test.csv')
+    c = CheckerGames(log, output)
     c.reviewCompetition(1, 1980)
     # Re-open output in read mode
     output.file = open('test.csv', 'r')
